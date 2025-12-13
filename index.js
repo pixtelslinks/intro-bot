027541180ca92d1c8c1942a50edd7c56dc9fd041
pixtelslinks <118294583+pixtelslinks@users.noreply.github.com>
2025-12-13 05:59:41 -0500

    init

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const { Client, GatewayIntentBits } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
 client.once('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('messageCreate', message => {
-  if (message.content === '!ping') {
-    message.reply('Pong!');
-  }
+    if (message.content === '!ping') {
+        message.reply('Pong!');
+    }
 });
 
 client.login(process.env.DISCORD_TOKEN);
